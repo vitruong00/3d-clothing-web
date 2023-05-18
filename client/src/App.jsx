492bb2ae6f0c96dcdf896b5e53f098ce60e3cae9
Vi Truong <vitruong@Vis-MacBook-Pro.hsd1.tx.comcast.net>
2023-05-17 21:20:18 -0500

    created all neccessary components, added animation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,15 @@
-import { useState } from "react";
+import Canvas from "./canvas";
+import Customizer from "./pages/Customizer";
+import Home from "./pages/Home";
 
 const App = () => {
-  const [count, setCount] = useState(0);
-
-  return <></>;
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
 };
 
 export default App;
